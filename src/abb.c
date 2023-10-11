343573f27383a6ebccb592985e3e7f114ec098d1
gimenezzeli <121507071+gimenezzeli@users.noreply.github.com>
2023-10-11 10:42:41 -0300

    Insertar y buscar

--- src/abb.c
+++ src/abb.c
@@ -3,42 +3,121 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-abb_t *abb_crear(abb_comparador comparador)
-{
-	return NULL;
+nodo_abb_t *crear_nodo(void *elemento){
+	nodo_abb_t *nodo_nuevo = malloc(sizeof(nodo_abb_t));
+
+	if(!nodo_nuevo){
+		free(nodo_nuevo);
+		return NULL;
+	}
+
+	nodo_nuevo->izquierda = NULL;
+	nodo_nuevo->derecha = NULL;
+	nodo_nuevo->elemento = elemento;
+
+	return nodo_nuevo;
 }
 
-abb_t *abb_insertar(abb_t *arbol, void *elemento)
-{
+abb_t *abb_crear(abb_comparador comparador){
+	abb_t *arbol = malloc(sizeof(abb_t));
+
+	if(!arbol || !comparador){
+		free(arbol);
+		return NULL;
+	}
+
+	arbol->nodo_raiz = NULL;
+	arbol->comparador = comparador;
+	arbol->tamanio = 0;
+
 	return arbol;
 }
 
-void *abb_quitar(abb_t *arbol, void *elemento)
-{
+nodo_abb_t *extraer_raiz(abb_t *arbol){
+	return arbol->nodo_raiz;
+}
+
+nodo_abb_t *insertar_rec(nodo_abb_t *raiz, void *elemento, int (*comparador)(void*, void*)){
+	if(!raiz){
+		raiz = crear_nodo(elemento);
+		return raiz;
+	}
+
+	int comparacion = comparador(elemento, raiz->elemento);
+
+	if(comparacion<=0){
+		raiz->izquierda = insertar_rec(raiz->izquierda, elemento, comparador);
+	} else {
+		raiz->derecha = insertar_rec(raiz->derecha, elemento, comparador);
+	}
+	
+	return raiz;
+}
+
+abb_t *abb_insertar(abb_t *arbol, void *elemento){
+	if(!arbol)
+		return NULL;
+
+	nodo_abb_t *raiz = extraer_raiz(arbol);
+
+	arbol->nodo_raiz = insertar_rec(raiz, elemento, arbol->comparador);
+	arbol->tamanio++;
+
+	return arbol;
+}
+
+void *abb_quitar(abb_t *arbol, void *elemento){
+
 	return elemento;
 }
 
-void *abb_buscar(abb_t *arbol, void *elemento)
-{
-	return elemento;
+void *buscar_rec(nodo_abb_t *raiz, void *elemento, int (*comparador)(void*, void*)){
+	if(!raiz)
+		return NULL;
+
+	int comparacion = comparador(elemento, raiz->elemento);
+
+	if(comparacion == 0)
+		return raiz->elemento;
+
+	if(comparacion<0)
+		return buscar_rec(raiz->izquierda, elemento, comparador);
+
+	return buscar_rec(raiz->derecha, elemento, comparador);
 }
 
-bool abb_vacio(abb_t *arbol)
-{
-	return true;
+void *abb_buscar(abb_t *arbol, void *elemento){
+	if(!arbol || !arbol->nodo_raiz)
+		return NULL;
+
+	nodo_abb_t *raiz = extraer_raiz(arbol);
+
+	void *elemento_buscado = buscar_rec(raiz, elemento, arbol->comparador);
+	if(!elemento_buscado)
+		return NULL;
+	
+	return elemento_buscado;
 }
 
-size_t abb_tamanio(abb_t *arbol)
-{
-	return 0;
+bool abb_vacio(abb_t *arbol){
+	return arbol->tamanio==0 || !arbol;
 }
 
-void abb_destruir(abb_t *arbol)
-{
+size_t abb_tamanio(abb_t *arbol){
+	return arbol->tamanio;
 }
 
-void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
-{
+void abb_destruir(abb_t *arbol){
+	// nodo_abb_t *raiz = extraer_raiz(arbol);
+	// abb_destruir(raiz->izquierda);
+	// abb_destruir(raiz->derecha);
+	// free(raiz);
+	free(arbol);
+}
+
+void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *)){
+	// if(!arbol || !destructor)
+	// 	return NULL;
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
